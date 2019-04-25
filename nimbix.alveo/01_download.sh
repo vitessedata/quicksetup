a35b98a89d7def78089ad17ad86b424575e95b57
Feng Tian <fengttt@gmail.com>
2019-04-25 09:52:21 -0700

    Updated to use 0419.

--- nimbix.alveo/01_download.sh
+++ nimbix.alveo/01_download.sh
@@ -5,6 +5,6 @@ source env.sh
 echo "Checking data dir $DATADIR ..."
 (mkdir -p $DATADIR && cd $DATADIR && touch ./.touch) && pass || fail
 
-wget https://s3.amazonaws.com/vitessedata/download/deepgreendb.18.16.ubuntu16.x86_64.190402.bin && pass || fail
+wget https://s3.amazonaws.com/vitessedata/download/deepgreendb.18.16.ubuntu16.x86_64.190419.bin && pass || fail
 wget https://dl.google.com/go/go1.9.5.linux-amd64.tar.gz && pass || fail
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz

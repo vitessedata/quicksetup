b0f8a66a7b492fc582dc7b7d2e0e118d5b5636b8
Feng Tian <fengttt@gmail.com>
2019-05-15 23:00:17 -0700

    u16.1525 is obsolete and unmaintained.

--- u16.1525/05_sql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source env.sh
-source ./deepgreendb/greenplum_path.sh
-
-dg setup -all $USER 
-psql -f ../sql/img.sql $USER
--- u16.1525/user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/groupadd deepgreen \
-    && /usr/sbin/useradd deepgreen -g deepgreen -r -m \
-    && echo deepgreen:deepGr33n | chpasswd \
-    && echo "deepgreen ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-

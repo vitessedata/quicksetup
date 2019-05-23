0200140582c3a9f3de4fa0e296422cac88019136
Feng Tian <fengttt@gmail.com>
2019-05-23 14:32:36 -0700

    Adding dockerhub_run.sh

--- docker/docker_dbg.sh
+++ docker/docker_dbg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+user=`whoami`
+
 sudo \
 docker run -it --rm \
     --hostname=dg --net=dgnet --ip=172.20.0.16 -p 8888:8888 \
@@ -7,5 +9,5 @@ docker run -it --rm \
 	-v /dev:/dev -v /opt/xilinx:/opt/xilinx \
 	-v /home/ftian/oss/quicksetup:/quicksetup \
 	--env PLATFORM=alveo-u200 \
-	ftian/xilinx-ml-suite:latest \
+	$user/xilinx-ml-suite:latest \
 /bin/bash -c "cd /home/mluser/quicksetup && git remote update && git rebase origin/master && cd /quicksetup && bash docker/start.sh"
--- docker/dockerhub_run.sh
+++ docker/dockerhub_run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-user=`whoami`
-
 sudo \
 docker run -it --rm --hostname=dg --net=dgnet --ip=172.20.0.16 --privileged -w /opt/ml-suite \
 	-v /dev:/dev -v /opt/xilinx:/opt/xilinx	\
 	--env PLATFORM=alveo-u200				\
-	$user/xilinx-ml-suite:latest \
+	vitessedata/deepgreen-ml:latest \
 /bin/bash -c "cd /home/mluser/quicksetup && git remote update && git rebase origin/master && bash docker/start.sh"
